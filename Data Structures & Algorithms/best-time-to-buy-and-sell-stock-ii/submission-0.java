5a6ed8237246a735c444a04d480bd7bce14b9359
Rahul Mamgain <rahul.mamgain@gmail.com>
2026-04-05 10:56:09 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int hold = -prices[0];
+        int cash = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            int prevCash = cash;
+            cash = Math.max(cash, hold + prices[i]);
+            hold = Math.max(hold, prevCash - prices[i]);
+        }
+
+        return cash;
+    }
+}
